59dc14259a853ea2f31e56dcd073ac7190760a41
GustavoAndreyIF <gustavo.andrey@escolar.ifrn.edu.br>
2025-10-14 15:37:45 -0300

    Configuração do ambiente e construição do header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ["./public/**/*.html"],
+    safelist: [
+        "bg-slate-800",
+        "bg-slate-900",
+        "text-slate-50",
+    ],
+	theme: {
+		extend: {
+		},
+	},
+	plugins: [],
+};
